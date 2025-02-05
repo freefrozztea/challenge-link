66ec7d490b0f21c0cb152707f869e0dc1de92ad1
Felipe Alzamora <luisfalmi@gmail.com>
2025-02-05 14:46:04 -0300

    add test directory

---
+++ src/test/java/demo/link_challenge/LinkChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.link_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
