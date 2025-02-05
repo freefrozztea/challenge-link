8122934e5623a469e46dee9221f1507092294560
Felipe Alzamora <luisfalmi@gmail.com>
2025-02-05 14:53:28 -0300

    add transaction controller

---
+++ src/main/java/demo/link_challenge/controllers/TransactionController.java
@@ -0,0 +1,31 @@
+package demo.link_challenge.controllers;
+
+import demo.link_challenge.dtos.TransactionDTO;
+import demo.link_challenge.services.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<TransactionDTO> createTransaction(@RequestBody TransactionDTO transactionDTO) {
+        return ResponseEntity.ok(transactionService.createTransaction(transactionDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TransactionDTO> getTransaction(@PathVariable Long id) {
+        return ResponseEntity.ok(transactionService.getTransaction(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionDTO>> getAllTransactions() {
+        return ResponseEntity.ok(transactionService.getAllTransactions());
+    }
+}
